418009ba35312f99d9fdeeb91bba2bc92cc4404b
S1rK` <talnakar5@gmail.com>
2020-01-05 20:29:35 +0200

    Added testing to 10-7

--- 10-7.py
+++ 10-7.py
@@ -32,18 +32,18 @@ def check_if_stable_marriage(first_side_pref: List[List[int]], second_side_pref:
 		first_pref = first_side_pref[a]
 		# get his marriage
 		married = get_other_couple(a, 0, marriage)
-
-		# go over a's prefs
+		# go over a's preferences
 		for b in first_pref:
-			# if b is the married, then there marriage is stable. go check the next ones.
+			# if b is the married, then their marriage is stable. go check the next ones.
 			if b is married:
-				continue
+				break
+
 			# if it's not
-			# get b's pref
+			# get b's preferences
 			second_pref = second_side_pref[b]
 			# get b's couple
 			bs_couple = get_other_couple(b, 1, marriage)
-			# check if b prefers a then his married couple
+			# if b prefers a more then his married couple, then it's not a stable
 			if second_pref.index(bs_couple) > second_pref.index(a):
 				return False
 		# every marriage is stable, return True
@@ -51,7 +51,41 @@ def check_if_stable_marriage(first_side_pref: List[List[int]], second_side_pref:
 
 
 def main():
-	check_if_stable_marriage()
+	print("-----------FIRST TEST-----------")
+	males = [[1, 2, 0], [0, 1, 2], [0, 2, 1]]
+	print("THE MALES PREFERENCES:")
+	print(males)
+	females = [[2, 1, 0], [1, 2, 0], [1, 0, 2]]
+	print("THE FEMALES PREFERENCES:")
+	print(females)
+	marriage = [(1, 0), (0, 1), (2, 2)]
+	print("THE MARRIAGE:")
+	print(marriage)
+	print("IS THE MARRIAGE IS STABLE: " + str(check_if_stable_marriage(males, females, marriage)))
+
+	print("\n\n-----------SECOND TEST-----------")
+	males = [[1, 2, 0], [0, 1, 2], [0, 2, 1]]
+	print("THE MALES PREFERENCES:")
+	print(males)
+	females = [[2, 1, 0], [1, 2, 0], [1, 0, 2]]
+	print("THE FEMALES PREFERENCES:")
+	print(females)
+	marriage = [(1, 2), (0, 1), (2, 0)]
+	print("THE MARRIAGE:")
+	print(marriage)
+	print("IS THE MARRIAGE IS STABLE: " + str(check_if_stable_marriage(males, females, marriage)))
+
+	print("\n\n-----------THIRD TEST-----------")
+	males = [[1, 2, 0], [0, 1, 2], [0, 2, 1]]
+	print("THE MALES PREFERENCES:")
+	print(males)
+	females = [[2, 1, 0], [1, 2, 0], [1, 0, 2]]
+	print("THE FEMALES PREFERENCES:")
+	print(females)
+	marriage = [(0, 0), (1, 1), (2, 2)]
+	print("THE MARRIAGE:")
+	print(marriage)
+	print("IS THE MARRIAGE IS STABLE: " + str(check_if_stable_marriage(males, females, marriage)))
 
 
 if __name__ == "__main__":
